aa330ffa333b4a45f6151b60342034744a3ebd73
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2023-02-09 16:09:57 -1000

    added comments for flight plan file parameters

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -9,28 +9,28 @@ using UnityEngine.UIElements;
 [System.Serializable]
 public class MissionItem
 {
-    public int AMSLAltAboveTerrain; // HACK: force null
+    public string type;    // "SimpleItem"
+    public int AMSLAltAboveTerrain; // Altitude value shown to the user. HACK: force null
     public double Altitude;
     public int AltitudeMode;
     public bool autoContinue;
-    public int command;
-    public int doJumpId;
-    public int frame;
+    public int command;    // MAV_CMD (16:MAV_CMD_NAV_WAYPOINT, 22:MAV_CMD_NAV_TAKEOFF)
+    public int doJumpId;   // The target id, auto-numbered from 1.
+    public int frame;      // MAV_FRAME (3:MAV_FRAME_GLOBAL_RELATIVE_ALT)
     public double[] param; // HACK: params is reserved
-    public string type;
 }
 
 [System.Serializable]
 public class Mission
 {
-    public int cruiseSpeed;
-    public int firmwareType;
-    public int globalPlanAltitudeMode;
-    public float hoverSpeed;
-    public int vehicleType;
-    public int version;
+    public int version;      // Current version is 2
+    public int firmwareType; // 12 = MAV_AUTOPILOT_PX4
+    public int vehicleType;  // 2 = MAV_TYPE_QUADROTOR
+    public int cruiseSpeed;  // The default forward speed for Fixed wing or VTOL vehicles
+    public float hoverSpeed; // The default forward speed for multi-rotor vehicles.
     public MissionItem[] items;
-    public double[] plannedHomePosition;
+    public double[] plannedHomePosition; // latitude, longitude and AMSL altitude
+    public int globalPlanAltitudeMode;
 }
 
 [System.Serializable]
@@ -51,12 +51,12 @@ public class RallyPoints
 [System.Serializable]
 public class FlightPlan
 {
-    public string fileType;
-    public GeoFence geoFence;
-    public string groundStation;
+    public string fileType;      // Must be "Plan"
+    public string groundStation; // The name of the ground station which created this file
+    public int version;          // Current version is 1
     public Mission mission;
+    public GeoFence geoFence;
     public RallyPoints rallyPoints;
-    public int version;
 }
 
 public struct GeoLocation
